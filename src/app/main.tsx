93ff6a66b944ae41313854659edca6160c5e5ef0
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-09-23 21:05:47 +0530

    tailwind + setups

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,51 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+import "./styles/globals.css";
+
+// Import the generated route tree
+import { routeTree } from "../routeTree.gen";
+
+import { queryClient } from "./lib/query-client";
+
+import Loader from "@/app/components/common/loader";
+import { NotFound } from "@/app/components/common/not-found";
+import { ErrorComponent } from "@/app/components/common/error-component";
+
+// Create a new router instance
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  context: {
+    queryClient,
+  },
+  Wrap: function WrapComponent({ children }: { children: React.ReactNode }) {
+    return (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    );
+  },
+  defaultPendingComponent: () => <Loader />,
+  defaultNotFoundComponent: NotFound,
+  defaultErrorComponent: ErrorComponent,
+});
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+// biome-ignore lint/style/noNonNullAssertion: <it will be there>
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import path from "path";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
+      routesDirectory: "./src/app/pages",
     }),
     react(),
     cloudflare(),
   ],
+  resolve: {
+    alias: {
+      "@/app": path.resolve(__dirname, "./src/app"),
+    },
+  },
 });
